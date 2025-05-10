fff57aca5d1444d24ede926a0fb200be4d9d717b
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-05-10 18:15:15 +0330

    having rounded top and bottom left border radius for sidebar

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -4,7 +4,7 @@ import Sidebar from "./Sidebar";
 function AppLayout({ children }) {
   return (
     <div className="flex h-screen">
-      <div className="w-48 shrink-0 border-l border-gray-200 bg-white p-4">
+      <div className="w-48 shrink-0 rounded-tl-2xl rounded-bl-2xl border-l border-gray-200 bg-white p-4">
         <Sidebar />
       </div>
       <main className="flex-1 overflow-y-auto p-6">{children}</main>
