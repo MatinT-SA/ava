ee6331c903662cb01b33c243bf91c7c92d219b64
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-05-10 17:31:27 +0330

    installing font vazir

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,6 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    fontFamily: {
-      sans: "Dangrek, Rubik, Poppins",
-    },
-
     extend: {
       height: {
         screen: "100dvh",
