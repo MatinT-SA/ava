d8d5e67927c6332200cf288c61fe16a8fee3d2e4
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-05-13 11:12:40 +0330

    adjusting link input and its icon

--- src/assets/icons/LinkIcon.jsx
+++ src/assets/icons/LinkIcon.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function LinkIcon({ className }) {
   return (
     <svg
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -4,15 +4,15 @@ import LinkIcon from "../assets/icons/LinkIcon";
 function Input() {
   return (
     <div className="relative w-full max-w-sm">
-      <div className="pointer-events-none absolute top-1/2 left-14 -translate-y-1/2">
-        <div className="flex h-8 w-8 items-center justify-center rounded-full bg-[#FF1654]">
-          <LinkIcon className="text-sm font-semibold text-white" />
+      <div className="pointer-events-none absolute top-1/2 left-13 -translate-y-1/2">
+        <div className="flex h-7.5 w-7.5 items-center justify-center rounded-full bg-[#FF1654]">
+          <LinkIcon className="h-4 w-3 text-white" />
         </div>
       </div>
       <input
         type="text"
         placeholder="example.com/sample.mp3"
-        className="w-10/12 rounded-full border border-[#FF1654] bg-white py-4 pl-16 text-left text-sm text-gray-700 placeholder-gray-400 shadow-sm transition focus:ring-1 focus:ring-[#FF1654] focus:outline-none"
+        className="w-10/12 rounded-full border border-[#FF1654] bg-white py-3 pl-15 text-left text-base text-gray-700 placeholder-gray-400 shadow-sm transition focus:ring-1 focus:ring-[#FF1654] focus:outline-none"
       />
     </div>
   );
