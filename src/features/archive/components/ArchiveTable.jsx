20403da15974b71e8503341b412d6e1ce3b225f2
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-05-13 13:02:36 +0330

    adjusting padding for table head and body

--- src/features/archive/components/ArchiveTable.jsx
+++ src/features/archive/components/ArchiveTable.jsx
@@ -16,16 +16,16 @@ export default function ArchiveTable() {
 
   return (
     <div className="overflow-x-auto rounded-xl bg-white px-12 py-6">
-      <h1 className="text-custom-teal mb-6 text-xl font-bold">آرشیو من</h1>
+      <h1 className="text-custom-teal mb-6 text-2xl">آرشیو من</h1>
 
       <table className="w-full border-separate border-spacing-y-4 text-right">
         <thead>
-          <tr className="rounded-md text-sm text-gray-600">
+          <tr className="font-bol rounded-md text-sm text-black">
             <th></th>
-            <th className="px-8 py-2 text-right">نام فایل</th>
-            <th className="px-4 py-2 text-right">تاریخ بارگذاری</th>
-            <th className="px-4 py-2 text-right">نوع فایل</th>
-            <th className="px-4 py-2 text-right">مدت زمان</th>
+            <th className="px-8 pt-4 pb-5 text-right font-bold">نام فایل</th>
+            <th className="px-4 text-right font-bold">تاریخ بارگذاری</th>
+            <th className="px-4 text-right font-bold">نوع فایل</th>
+            <th className="px-4 text-right font-bold">مدت زمان</th>
           </tr>
         </thead>
         <tbody>
